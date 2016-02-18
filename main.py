ba9f390faf675732ff9867cb25ef52133f49117e
rytai <thompso0@hotmail.com>
2016-02-18 16:13:24 +0200

    Ensimmäinen crest haku + linkki json tutoon.

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import subprocess
 import os
+import platform  # To find out whether we're on window, or linux
+import urllib2
+import urllib
 
-import pycrest
+import pycrest  # Api to connect to Crest
+
+linux_or_windows = platform.system()
+
+if linux_or_windows != 'Linux' and linux_or_windows != 'Windows':
+    print '''This script can only be run with linux or windows system.'
+          Your system is: {}.'''.format(linux_or_windows)
 
 
+# Sound alaaarm!
 def sound_alarm():
-    with open(os.devnull, "wb") as f:
-        subprocess.call(['/bin/sh', '-c', 'aplay warning_sound.wav'], stdout=f, stderr=f)
+    if linux_or_windows == 'Linux':
+        with open(os.devnull, "wb") as f:
+            subprocess.call(['/bin/sh', '-c', 'aplay warning_sound.wav'], stdout=f, stderr=f)
 
 
 def main():
@@ -14,6 +25,14 @@ def main():
     eve()
 
     print eve.motd
+    print eve.regions
+    # The Forge region ID is 10000002, from eve.regions
+    urllib.urlretrieve("https://public-crest.eveonline.com/regions/10000002/", "temp_forgedata")
+    data = urllib2.urlopen('https://public-crest.eveonline.com/regions/10000002/')
+    print data.read()
+
+    # Next up: https://docs.python.org/2/library/json.html
+
 
     sound_alarm()
 
