051c81b5875e69d61684b4f70099852af907e326
rytai <thompso0@hotmail.com>
2016-02-19 20:50:32 +0200

    parsing solarsystemsjumps file

--- parse_sde.py
+++ parse_sde.py
@@ -1,5 +1,31 @@
 with open('mapSolarSystemJumps') as f:
 
+    f.readline()
+
+    jumps = {}
+
+    from_jump = None
+    column = 1
+    sentence = ''
+
     for line in f.readlines():
-        rivi = line[:-2]
-        print rivi
+
+
+        for symbol in line[:-2]:
+            if symbol == ',':
+                column += 1
+                if column > 6:
+                    column = 1
+
+                if column == 3:
+                    from_jump = sentence
+
+                if column == 6:
+                    to_jump = sentence
+                    jumps[from_jump] = to_jump
+
+                sentence = ''
+            else:
+                sentence+=symbol
+
+print jumps.items()
