15f990182a34a853fd2f90940adc2867e8485c39
rytai <thompso0@hotmail.com>
2016-02-19 20:12:08 +0200

    Testing PyCrest api.

--- main.py
+++ main.py
@@ -6,35 +6,43 @@ import urllib
 
 import pycrest  # Api to connect to Crest
 
+import requests
+import json  # For parsing the received crest data.
+
 linux_or_windows = platform.system()
 
 if linux_or_windows != 'Linux' and linux_or_windows != 'Windows':
     print '''This script can only be run with linux or windows system.'
           Your system is: {}.'''.format(linux_or_windows)
+    exit()
 
 
-# Sound alaaarm!
+# Multi-platfrom warning sound when needed.
 def sound_alarm():
     if linux_or_windows == 'Linux':
         with open(os.devnull, "wb") as f:
             subprocess.call(['/bin/sh', '-c', 'aplay warning_sound.wav'], stdout=f, stderr=f)
+
+def getByAttrVal(objlist, attr, val):
+    ''' Searches list of dicts for a dict with dict[attr] == val '''
+    matches = [getattr(obj, attr) == val for obj in objlist]
+    index = matches.index(True)  # find first match, raise ValueError if not found
+    return objlist[index]
 
 
 def main():
     eve = pycrest.EVE()
     eve()
 
-    print eve.motd
-    print eve.regions
-    # The Forge region ID is 10000002, from eve.regions
-    urllib.urlretrieve("https://public-crest.eveonline.com/regions/10000002/", "temp_forgedata")
-    data = urllib2.urlopen('https://public-crest.eveonline.com/regions/10000002/')
-    print data.read()
+    print 'Jita by name'+str(getByAttrVal(eve.systems().items, 'name', 'Jita'))
 
-    # Next up: https://docs.python.org/2/library/json.html
+    jita = getByAttrVal(eve.systems().items, 'id', 30000142)
 
+    print 'Jita info:'
+    for line in str(jita()).split(','):
+        print line
 
-    sound_alarm()
+    #sound_alarm()
 
 
 if __name__ == '__main__':
