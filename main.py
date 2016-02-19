2a69d6f7f9850869e053ce852abbcc9c6ce04457
rytai <thompso0@hotmail.com>
2016-02-19 21:47:19 +0200

    Got neighbour systems correctly

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import urllib2
 import urllib
 
 import pycrest  # Api to connect to Crest
+import parse_sde
 
 import requests
 import json  # For parsing the received crest data.
@@ -23,6 +24,7 @@ def sound_alarm():
         with open(os.devnull, "wb") as f:
             subprocess.call(['/bin/sh', '-c', 'aplay warning_sound.wav'], stdout=f, stderr=f)
 
+
 def getByAttrVal(objlist, attr, val):
     ''' Searches list of dicts for a dict with dict[attr] == val '''
     matches = [getattr(obj, attr) == val for obj in objlist]
@@ -30,19 +32,66 @@ def getByAttrVal(objlist, attr, val):
     return objlist[index]
 
 
+def find_solar_system_neighbours(jumps_dictionary, current_system):
+    assert isinstance(jumps_dictionary, dict)
+    assert isinstance(current_system, str)
+    list_of_neighbour_ids = []
+    try:
+        list_of_neighbour_ids = jumps_dictionary[current_system]
+    except KeyError, k_error:
+        print k_error
+    return list_of_neighbour_ids
+
+
+def get_solar_system_by_id(eve, id):
+    return getByAttrVal(eve.systems().items, 'id', int(id))
+
+
 def main():
     eve = pycrest.EVE()
     eve()
 
-    print 'Jita by name'+str(getByAttrVal(eve.systems().items, 'name', 'Jita'))
+    print 'Jita by name' + str(getByAttrVal(eve.systems().items, 'name', 'Jita'))
 
     jita = getByAttrVal(eve.systems().items, 'id', 30000142)
 
-    print 'Jita info:'
+    #  print 'Jita info:'
     for line in str(jita()).split(','):
-        print line
+        #print line
+        pass
 
-    #sound_alarm()
+    # Get dict containing all the possible jumps between systems.
+    jumps_dictionary = parse_sde.parse_sde()  # Jita neighbours
+
+    immediate_neighbours_ids = find_solar_system_neighbours(jumps_dictionary, '30000142')
+    immediate_neighbours = []
+
+    for solar_system_id in immediate_neighbours_ids:
+        new_system = get_solar_system_by_id(eve, solar_system_id)
+        immediate_neighbours.append(new_system)
+
+    # Get ID's from one jump away. no duplicates
+    one_jump_neighbour_ids = []
+    for neighbour_id in immediate_neighbours_ids:
+        for one_jump_neighbour_id in find_solar_system_neighbours(jumps_dictionary, neighbour_id):
+            if one_jump_neighbour_id not in immediate_neighbours_ids:
+                if one_jump_neighbour_id not in one_jump_neighbour_ids:
+                    one_jump_neighbour_ids.append(one_jump_neighbour_id)
+
+    # Get system names from the id's
+    one_jump_neighbours = []
+    for one_jump_neighbour_id in one_jump_neighbour_ids:
+        new_system = get_solar_system_by_id(eve, one_jump_neighbour_id)
+        one_jump_neighbours.append(new_system)
+
+    print "Jita neighbours:--------------------------------------------"
+    for system in immediate_neighbours:
+        print system.name
+    print "2 jumps from jita:------------------------------------------"
+    for system in one_jump_neighbours:
+        print system.name
+
+        # sound_alarm()
 
 
 if __name__ == '__main__':
--- parse_sde.py
+++ parse_sde.py
@@ -1,31 +1,43 @@
-with open('mapSolarSystemJumps') as f:
+def parse_sde():
+    with open('mapSolarSystemJumps') as f:
 
-    f.readline()
+        f.readline()
 
-    jumps = {}
+        jumps = {}
 
-    from_jump = None
-    column = 1
-    sentence = ''
+        from_jump = None
+        column = 1
+        sentence = ''
 
-    for line in f.readlines():
+        for line in f.readlines():
 
+            for symbol in line[:-2]:
+                # Separator
+                if symbol == ',':
 
-        for symbol in line[:-2]:
-            if symbol == ',':
-                column += 1
-                if column > 6:
-                    column = 1
+                    # Found from_solar_system jump
+                    if column == 3:
+                        from_jump = sentence
 
-                if column == 3:
-                    from_jump = sentence
+                    # Found to_solar_system jump
+                    if column == 4:
+                        to_jump = sentence
+                        # Try if a jump already exists.
+                        try:
+                            jumps[from_jump].append(to_jump)
+                        except:  # Create list containing this jump.
+                            jumps[from_jump] = [to_jump]
 
-                if column == 6:
-                    to_jump = sentence
-                    jumps[from_jump] = to_jump
+                    sentence = ''
+                    column += 1
+                    if column > 6:
+                        column = 1
+                elif symbol == '"':
+                    pass
+                else:
+                    sentence += symbol
 
-                sentence = ''
-            else:
-                sentence+=symbol
+            sentence = ''
+            column = 1
 
-print jumps.items()
+    return jumps
